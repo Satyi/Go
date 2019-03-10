044faa64ad6963a029308d1795f2a39bb2bd6b2d
Satyi <goelsatyam750@gmail.com>
2019-03-10 12:30:24 +0530

    Fisrt

---
+++ Proj/src/com/javatpoint/Hello.java
@@ -0,0 +1,9 @@
+package com.javatpoint;
+
+import java.util.logging.Logger;
+
+public class Hello {
+public static void main(String[] args) {
+Logger.getLogger("hello");
+}
+}
---
+++ Sei1/src/Ex/Serv.java
@@ -0,0 +1,43 @@
+package Ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Serv
+ */
+@WebServlet("/Serv")
+public class Serv extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Serv() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Jav j=new Jav();
+		String st1=j.m1();
+		String st2=j.m2();
+	request.setAttribute("hi", st1);
+	request.setAttribute("h1", st2);
+	request.getRequestDispatcher("File.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
